7bb27233611e4e1ee73ceb8ec1ab23c5f9d2068d
de4rm <graurvadim91@gmail.com>
2018-03-31 21:22:16 +0100

    created Class Link with id, url_user, url_generated

--- app/models.py
+++ app/models.py
@@ -1,1 +1,9 @@
 from app import db
+
+class Link(db.Model):
+	id = db.Column(db.Integer,primary_key=True)
+	url_user = db.Column(db.String(120), index=True, unique=True)
+	url_generated = db.Column(db.String(8), index=True, unique=True)
+
+	def _repr__(self):
+		return "Link, {}".format(self.url_user)
