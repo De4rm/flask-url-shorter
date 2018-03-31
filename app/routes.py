e973196ff772a3c403af2be18c8922fa00e0062d
de4rm <graurvadim91@gmail.com>
2018-03-31 20:53:23 +0100

    adding render_template to index page

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
 from flask import render_template
 from app import app
+from app.forms import ShortForm
 
 @app.route("/", methods=["GET","POST"])
 def index():
-	return "Hello"
+	form = ShortForm()
+	if form.validate_on_submit():
+		pass
+	return render_template('index.html',form=form)
