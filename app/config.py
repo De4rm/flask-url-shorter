a639d928b47d4248d807075f0d8cd377a6f6f1e3
de4rm <graurvadim91@gmail.com>
2018-03-31 21:43:41 +0100

    add SQLALCHEMY_TRACK_MODIFICATIONS to config.py

--- app/config.py
+++ app/config.py
@@ -5,3 +5,4 @@ currdir = os.path.dirname(os.path.realpath(__file__))
 class Config(object):
 	SECRET_KEY = "this is secret key from de4rm"
 	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(currdir,"bd.sqlite")
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
