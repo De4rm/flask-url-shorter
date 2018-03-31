e6188c14c5f34466ef9bc0f41b556bbec2eede6d
de4rm <graurvadim91@gmail.com>
2018-03-31 21:13:10 +0100

    modified index.html added form

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from app.config import Config
 
 app = Flask(__name__)
 
-from app import routes
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+
+from app import routes, models
