ab8eb585eac1dfbda86afcb92c22f889ade05488
de4rm <graurvadim91@gmail.com>
2018-03-31 21:40:12 +0100

    adding SQLAlchemy_database_uri

--- app/config.py
+++ app/config.py
@@ -1,4 +1,7 @@
+import os
 
+currdir = os.path.dirname(os.path.realpath(__file__))
 
 class Config(object):
-	SECRET_KEY = "this is secret key from de4rm"	
+	SECRET_KEY = "this is secret key from de4rm"
+	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(currdir,"bd.sqlite")
